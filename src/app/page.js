6f981c3f8f57a82b77473bfc2bb333ed62dbd3f4
muhammad wildan iskandar <muhammadwildaniskandar@gmail.com>
2024-03-21 10:53:33 +0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,153 @@
-import Image from "next/image";
+"use client"
+import {useEffect, useState} from "react";
+import {cekKodeMesin, getDaftarInstansi, getKodeMesin, putKodeMesin} from "@/lib/api";
+import KodeMesinDialog from "@/components/KodeMesinDialog";
+import {ThemeToggle} from "@/components/ui/theme-toggle";
+import {Card, CardContent} from "@/components/ui/card";
+import {useMutation, useQuery} from "@tanstack/react-query";
+import LayananItem from "@/components/LayananItem";
+import ImageBackgorund from "@/components/ImageBackgorund";
+import {DetailLayananDialog} from "@/components/DetailLayananDialog";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {useToast} from "@/components/ui/use-toast";
+import AKThermalPrint from "@/lib/AKThermalPrint";
+import {connectToPrinter, getConnectedDevices} from "@/lib/utils";
+import PrinterSetting from "@/components/PrinterSetting";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    // State variables
+    const {toast} = useToast()
+    const [kodeMesin, setKodeMesin] = useState(null);
+    const [dataLayanan, setDataLayanan] = useState([]);
+    const [isLoading, setIsLoadin] = useState(null);
+    const [clickedLayanan, setClickedLayanan] = useState(null);
+    const [deviceThermal, setDeviceThermal] = useState(null)
+    const [time, setTime] = useState({
+        minutes: new Date().getMinutes(),
+        hours: new Date().getHours(),
+        seconds: new Date().getSeconds()
+    });
+    const [currentDate, setCurrentDate] = useState('');
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    // Query for fetching data
+    // const cctvQuery = useQuery({ queryKey: ['get_cctv'], queryFn: getDaftarInstansi });
+    // const { data: dataCCTV, isLoading1, isError: queryError } = cctvQuery;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const daftarInstansiMutation = useMutation({
+        mutationFn: getDaftarInstansi,
+        onMutate: () => {
+            setIsLoadin(true);
+        },
+        onError: (error) => {
+            setIsLoadin(false);
+            toast({
+                variant: "destructive",
+                title: "Oopss, Telah Terjadi Kesalahan!",
+                description: error.message,
+            })
+        },
+        onSuccess: (data) => {
+            setIsLoadin(false);
+            setDataLayanan(data)
+        },
+    });
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    useEffect(() => {
+        if (kodeMesin !== null && dataLayanan.length === 0)
+            daftarInstansiMutation.mutate({kode: kodeMesin})
+    }, [kodeMesin]);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    // Function to format numbers to two digits
+    const convertToTwoDigit = (number) => {
+        return number.toLocaleString('en-US', {
+            minimumIntegerDigits: 2
+        });
+    };
+
+    // Function to update current date
+    const updateCurrentDate = () => {
+        const date = new Date();
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            timeZone: 'Asia/Jakarta', // Set the time zone to Indonesia
+            locale: 'id-ID' // Set the locale to Indonesian (Bahasa Indonesia)
+        };
+        setCurrentDate(date.toLocaleDateString('id-ID', options));
+    };
+
+
+    // useEffect to update time every second
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const date = new Date();
+            setTime({
+                minutes: date.getMinutes(),
+                hours: date.getHours(),
+                seconds: date.getSeconds()
+            });
+            updateCurrentDate(); // Update current date every second
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    // useEffect to get kodeMesin once
+    useEffect(() => {
+        setKodeMesin(getKodeMesin());
+
+    }, []);
+
+    // Function to handle successful kodeMesin setting
+    const successKodeHandler = ({kode}) => {
+        putKodeMesin(kode);
+        setKodeMesin(kode);
+    };
+
+    return (
+        <>
+            <ThemeToggle/>
+            <PrinterSetting onSuksesSet={(test) => setDeviceThermal(test)} device={deviceThermal ? deviceThermal.device : null}/>
+            <main className="flex flex-col gap-12 p-16">
+                <div className='self-center font-extrabold flex flex-row text-xl gap-2'>
+                    <div>{currentDate}</div>
+                    <div>
+                        <span>{convertToTwoDigit(time.hours)}:</span>
+                        <span>{convertToTwoDigit(time.minutes)}:</span>
+                        <span>{convertToTwoDigit(time.seconds)}</span>
+                    </div>
+                </div>
+                {isLoading && <h1>Sedang Memuat Data!</h1>}
+                {kodeMesin === null && <KodeMesinDialog onSuccessSetKode={successKodeHandler}/>}
+                {clickedLayanan !== null &&
+                    <DetailLayananDialog thermalDevice={deviceThermal.device} deviceOutPath={deviceThermal.deviceOutEndpoint}
+                                         dataLayanan={clickedLayanan} onClose={() => setClickedLayanan(null)}/>}
+                <div className=" absolute left-4 top-4 flex flex-row items-center gap-2 max-w-sm">
+                    <img src="/tabalong.png" alt="logo kominfo" className="h-auto w-8"/>
+                    <div className="flex-row">
+                        <p className="font-bold text-black dark:text-white text-sm">MAL PELAYANAN PUBLIK</p>
+                        <p className="font-bold text-black dark:text-white text-sm">KABUPATEN TABALONG</p>
+                    </div>
+                </div>
+                {dataLayanan && <div className="grid grid-cols-2 gap-7">
+                    {dataLayanan.map((layanan) => (
+                        <LayananItem key={layanan.id} nama={layanan.nama}
+                                     onLayananClick={() => setClickedLayanan(layanan)}/>
+                    ))}
+                </div>}
+                {/*{dataCCTV && <div className="grid grid-cols-2 gap-7">*/}
+                {/*    {dataCCTV.map((layanan) => (*/}
+                {/*        <LayananItem key={layanan.id} nama={layanan.nama}*/}
+                {/*                     onLayananClick={() => setClickedLayanan(layanan)}/>*/}
+                {/*    ))}*/}
+                {/*</div>}*/}
+
+            </main>
+        </>
+
+    );
 }

1ad5e67e253459980b8c8a408cce105399b9110e
muhammad wildan iskandar <muhammadwildaniskandar@gmail.com>
2024-08-26 08:59:12 +0800

    change socket domain

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,90 +1,95 @@
-const baseUrl = "https://socket.antrian-mpp.tabalongkab.go.id"
+const baseUrl = "https://socket-antrian-mpp.tabalongkab.go.id";
 // const baseUrl = "http://localhost:11081"
 
 function getKodeMesin() {
-    return localStorage.getItem('CONFIG_KODE_MESIN');
+  return localStorage.getItem("CONFIG_KODE_MESIN");
 }
 
 function putKodeMesin(kode) {
-    return localStorage.setItem('CONFIG_KODE_MESIN', kode);
+  return localStorage.setItem("CONFIG_KODE_MESIN", kode);
 }
 
-async function getDaftarInstansi({kode}) {
-    // const response = await fetch('./dummy.json');
-    const response = await fetch(`${baseUrl}/kiosk/beranda`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            kode,
-        }),
-    });
+async function getDaftarInstansi({ kode }) {
+  // const response = await fetch('./dummy.json');
+  const response = await fetch(`${baseUrl}/kiosk/beranda`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      kode,
+    }),
+  });
 
-    const responseJson = await response.json();
-    const {success, message} = responseJson;
+  const responseJson = await response.json();
+  const { success, message } = responseJson;
 
-    if (!success) {
-        throw new Error(message);
-    }
+  if (!success) {
+    throw new Error(message);
+  }
 
-    const {data: {instansi}} = responseJson;
+  const {
+    data: { instansi },
+  } = responseJson;
 
-
-    return instansi;
+  return instansi;
 }
 
-async function cekKodeMesin({kode}) {
-    const response = await fetch(`${baseUrl}/kiosk/cek-mesin`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            kode,
-        }),
-    });
+async function cekKodeMesin({ kode }) {
+  const response = await fetch(`${baseUrl}/kiosk/cek-mesin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      kode,
+    }),
+  });
 
-    const responseJson = await response.json();
-    const {success, message} = responseJson;
+  const responseJson = await response.json();
+  const { success, message } = responseJson;
 
-    if (!success) {
-        throw new Error(message);
-    }
+  if (!success) {
+    throw new Error(message);
+  }
 
-    const {data: {kiosk}} = responseJson;
+  const {
+    data: { kiosk },
+  } = responseJson;
 
-    return kiosk;
+  return kiosk;
 }
 
-async function ambilAntrian({kode, layananId}) {
-    const response = await fetch(`${baseUrl}/kiosk/antrian/ambil`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            kode,
-            layanan: layananId
-        }),
-    });
+async function ambilAntrian({ kode, layananId }) {
+  const response = await fetch(`${baseUrl}/kiosk/antrian/ambil`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      kode,
+      layanan: layananId,
+    }),
+  });
 
-    const responseJson = await response.json();
-    const {success, message} = responseJson;
+  const responseJson = await response.json();
+  const { success, message } = responseJson;
 
-    if (!success) {
-        throw new Error(message);
-    }
+  if (!success) {
+    throw new Error(message);
+  }
 
-    const {data: {antrian}} = responseJson;
+  const {
+    data: { antrian },
+  } = responseJson;
 
-    return antrian;
+  return antrian;
 }
 
 export {
-    getKodeMesin,
-    putKodeMesin,
-    getDaftarInstansi,
-    cekKodeMesin,
-    ambilAntrian
+  ambilAntrian,
+  cekKodeMesin,
+  getDaftarInstansi,
+  getKodeMesin,
+  putKodeMesin,
 };
